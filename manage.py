6b1f109556551b93674bfd5cbbf70100f82de464
indra4837 <indra4837@gmail.com>
2021-12-13 22:36:15 +0100

    added apidocs for api endpoints

--- manage.py
+++ manage.py
@@ -3,8 +3,10 @@ import os
 from flask_migrate import Migrate
 from app import db, create_app
 from app import models
+from flask_apidoc.commands import GenerateApiDoc
 
 
 app = create_app(config_name=os.getenv("APP_SETTINGS"))
+app.cli.add_command(GenerateApiDoc(), "apidoc")
 migrate = Migrate(app, db)
 migrate.init_app(app, db)
